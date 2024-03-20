2c9e1790b79938741beaf4190e6e13bd5ddf3721
nageri <onacedricnageri@gmail.com>
2024-03-20 15:03:53 +0300

    fix:   - modified morgan logger   - changed login logic,     User is redirected to /login if not logged in.   - removed irrelevant packages.

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import "dotenv/config";
 import cors from "cors";
 import morgan from "morgan";
 import express from "express";
-import uuid from "node-uuid";
-import { LocalStorage } from "node-localstorage";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -12,7 +10,6 @@ import courseRouter from "./routes/courses.js";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(assignId);
 app.use(cors());
 const PORT = process.env.PORT || 4000;
 
@@ -21,25 +18,19 @@ if (process.env.NODE_ENV == "development") {
   morgan.token("host", function (req, res) {
     return req.hostname;
   });
-  morgan.token("id", function getId(req) {
-    return req.id;
-  });
+  // ":id :method :host :status :res[content-length] :date[web] :response-time ms"
   app.use(
     morgan(
-      ":id :method :host :status :res[content-length] :date[web] :response-time ms"
+      ":remote-addr ':host'  :remote-user [:date[clf]] ':method :url HTTP/:http-version' :status :response-time ms - :res[content-length] ':referrer'"
     )
   );
 } else {
   var BASE_URL = process.env.BASE_URL;
 }
-function assignId(req, res, next) {
-  req.id = uuid.v4();
-  next();
-}
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-var localStorage = new LocalStorage("./scratch");
+let loggedIn = false;
 
 app.get("/", function (req, res) {
   res.send("Go To /login");
@@ -63,19 +54,18 @@ app.post("/login", (req, res) => {
       .send("Access denied - Password or Username is incorrect!");
   }
 
-  localStorage.setItem("loggedIn", "OK");
+  loggedIn = true;
   res.redirect("/node-course");
 });
 
 app.get("/node-course", (req, res) => {
   try {
-    const loggedIn = localStorage.getItem("loggedIn");
-    if (loggedIn === "OK") {
-      res.sendFile(path.join(__dirname, "Public", "node-course.html"));
+    if (loggedIn) {
+      return res.sendFile(path.join(__dirname, "Public", "node-course.html"));
     }
+    res.redirect("/login");
   } catch (error) {
     console.error(error);
-    res.redirect("/login");
   }
 });
 app.use("/api/courses", courseRouter);
